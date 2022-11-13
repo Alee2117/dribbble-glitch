b8866765062c979e7ad1bb5430dbd4ae6138d558
Austin Lee <austinlee@DESKTOP-HARPV5M.localdomain>
2022-11-13 18:17:17 -0500

    added alittle content and more styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,29 @@ export default function Home() {
     <div className={styles.container}>
       <div className={styles["main-window"]}>
         <WindowOptions />
-       <Navbar />
+        <Navbar />
       </div>
       <div className={styles["tiny-window"]}>
         <WindowOptions />
-       <Navbar />
+        <Navbar />
+      </div>
+      <div className={styles["md-text-window-three"]}>
+        <WindowOptions />
+        <Navbar />
+        <div className={styles["glitch-text"]}>HERE</div>
+      </div>
+      <div className={styles["md-text-window-two"]}>
+        <WindowOptions />
+        <Navbar />
+        <div className={styles["glitch-text"]}>STARTS</div>
+      </div>
+      <div className={styles["md-text-window-one"]}>
+        <WindowOptions />
+        <Navbar />
+        <div className={styles["glitch-text"]}>CHANGE</div>
+      </div>
+      <div className={styles["black-window"]}>
+        
       </div>
     </div>
   )
