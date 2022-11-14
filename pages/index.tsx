6cfa0a0a164c3a70a5072e797d3e249c83f4a273
Austin Lee <alee2117@gmail.com>
2022-11-13 21:17:15 -0500

    Added animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,36 +3,65 @@ import Image from "next/image"
 import Navbar from "../components/Navbar/Navbar"
 import WindowOptions from "../components/WindowOptions/WindowOptions"
 import styles from "../styles/Home.module.scss"
+import { motion } from "framer-motion"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles["main-window"]}>
+      <motion.div
+        className={styles["main-window"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+      >
         <WindowOptions />
         <Navbar />
-      </div>
-      <div className={styles["tiny-window"]}>
+      </motion.div>
+      <motion.div
+        className={styles["tiny-window"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <WindowOptions />
         <Navbar />
-      </div>
-      <div className={styles["md-text-window-three"]}>
+      </motion.div>
+      <motion.div
+        className={styles["md-text-window-three"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
         <WindowOptions />
         <Navbar />
         <div className={styles["glitch-text"]}>HERE</div>
-      </div>
-      <div className={styles["md-text-window-two"]}>
+      </motion.div>
+      <motion.div
+        className={styles["md-text-window-two"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
         <WindowOptions />
         <Navbar />
         <div className={styles["glitch-text"]}>STARTS</div>
-      </div>
-      <div className={styles["md-text-window-one"]}>
+      </motion.div>
+      <motion.div
+        className={styles["md-text-window-one"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 2 }}
+      >
         <WindowOptions />
         <Navbar />
         <div className={styles["glitch-text"]}>CHANGE</div>
-      </div>
-      <div className={styles["black-window"]}>
-        
-      </div>
+      </motion.div>
+      <motion.div
+        className={styles["black-window"]}
+        initial={{ opacity: 0, scale: 0.1 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 2.5 }}
+      ></motion.div>
     </div>
   )
 }
